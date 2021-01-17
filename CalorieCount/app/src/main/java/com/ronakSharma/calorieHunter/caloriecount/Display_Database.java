54535de023b36ce3b38a6e63ef7524c305fdeea9
Ronak Sharma <rsharma10@sheffield.ac.uk>
2021-01-17 16:55:12 +0000

    Updated dependencies to publish app in PlayStore

---
+++ CalorieCount/app/src/main/java/com/ronakSharma/calorieHunter/caloriecount/Display_Database.java
@@ -0,0 +1,67 @@
+package com.ronakSharma.calorieHunter.caloriecount;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+//----------PRINTS THE DATABASE IN A NEW ACTIVITY FOR THE USER TO VIEW-------------//
+public class Display_Database extends AppCompatActivity {
+
+    userInfoDatabaseHandler databaseHandler;
+
+    //- Transition to the next activity--//
+    public void onClickBack(View view) {
+        Intent transitionToDayOneInterface= new Intent(this,dayOneInterface.class);
+        startActivity(transitionToDayOneInterface);
+    }
+    public void onClickDisplayData(View view) {
+        displayDatabase();
+    }
+
+//-----Displays the database--------//
+    public void displayDatabase() {
+
+        TextView txtFoodName = (TextView) findViewById(R.id.txtFoodName);
+        TextView txtCaloriePerGram = (TextView) findViewById(R.id.txtCaloriePerGram);
+        TextView txtAmountConsumed = (TextView) findViewById(R.id.txtAmountConsumed);
+        TextView txtCalorieIntake = (TextView) findViewById(R.id.txtCalorieIntake);
+        String FoodName = "";
+        String CaloriePerGram = "";
+        String AmountConsumed = "";
+        String CalorieIntake = "";
+
+        //------ACCESS DATABASE-----------//
+        String dbString = databaseHandler.databaseToString();
+        String[] separatedDatabase = dbString.split(":"); // Delimits the string received (More info in 'userInfoDatabase' or 'dayOneInterface')
+        int separatedDatabaseSize = separatedDatabase.length;
+
+        //--------DISPLAY DATABASE INFORMATION---------//
+        //-- This function loops through all the user entries, stores the 0,4,8, element in one array (Food Name), the next 1,5,9 in another array (Calorieper100G) etc----//
+        // The loop so iterates every 4 times and each time it iterates, it stores the first input (row) entered by the user and the entries in individual arrays
+        for (int i = 0; i <= separatedDatabaseSize-4; i+=4) {
+
+            FoodName+=separatedDatabase[i]; //IMPORTANT: The first one is not added '\n' as this already exists in the printDatabase command to add '\n' at the end, so don't need anymore
+            CaloriePerGram+=separatedDatabase[i+1]+"\n";
+            AmountConsumed+=separatedDatabase[i+2]+"\n";
+            CalorieIntake+=separatedDatabase[i+3]+"\n";
+        }
+
+        txtFoodName.setText(FoodName);
+        txtCaloriePerGram.setText(CaloriePerGram);
+        txtAmountConsumed.setText(AmountConsumed);
+        txtCalorieIntake.setText(CalorieIntake);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display__database);
+        databaseHandler=new userInfoDatabaseHandler(this,null,null,5); // Connects to the right database automatically
+
+    }
+
+
+}
---
+++ CalorieCount/app/src/main/java/com/ronakSharma/calorieHunter/caloriecount/GraphNetCalorie.java
@@ -0,0 +1,116 @@
+package com.ronakSharma.calorieHunter.caloriecount;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphNetCalorie extends AppCompatActivity {
+
+
+    // DISPLAYS A GRAPH SHOWING THE CALORIE CONSUMED EACH DAY//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_net_calorie);
+        //----------GETS ALL THE VALUES OF CALORIES CONSUMED-----  -//
+        SharedPreferences netCalorieDaily = getSharedPreferences("totalCalorieConsumed", Context.MODE_PRIVATE);
+        String netCalorieDay1 = netCalorieDaily.getString("TotalCalorieDay1", "0");
+        String netCalorieDay2 = netCalorieDaily.getString("TotalCalorieDay2", "0");
+        String netCalorieDay3 = netCalorieDaily.getString("TotalCalorieDay3", "0");
+        String netCalorieDay4 = netCalorieDaily.getString("TotalCalorieDay4", "0");
+        String netCalorieDay5 = netCalorieDaily.getString("TotalCalorieDay5", "0");
+        String netCalorieDay6 = netCalorieDaily.getString("TotalCalorieDay6", "0");
+        String netCalorieDay7 = netCalorieDaily.getString("TotalCalorieDay7", "0");
+
+
+        BarChart chart = (BarChart) findViewById(R.id.chart);
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1f, Float.valueOf(netCalorieDay1)));
+        entries.add(new BarEntry(2f, Float.valueOf(netCalorieDay2)));
+        entries.add(new BarEntry(3f, Float.valueOf(netCalorieDay3)));
+        entries.add(new BarEntry(4f, Float.valueOf(netCalorieDay4)));
+        entries.add(new BarEntry(5f, Float.valueOf(netCalorieDay5)));
+        entries.add(new BarEntry(6f, Float.valueOf(netCalorieDay6)));
+        entries.add(new BarEntry(7f, Float.valueOf(netCalorieDay7)));
+
+        //-----------DESIGN------------//
+        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+        BarData data = new BarData(set);
+        data.setValueTextSize(15f);
+        data.setBarWidth(0.9f); // set custom bar width
+        chart.setData(data);
+        chart.setFitBars(true); // make the x-axis fit exactly all bars
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setPinchZoom(true);
+        chart.setDoubleTapToZoomEnabled(true);
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.CYAN);
+        colors.add(Color.YELLOW);
+
+        //X- AXIS Settings
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(15f);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(true);
+        //xAxis.setText("Date");
+        //Y-AXIS Settings
+        YAxis left = chart.getAxisLeft();
+        //LimitLine ll = new LimitLine(20f, "Calories");
+
+        left.setDrawZeroLine(false); // draw a zero line
+        left.setDrawLabels(true); // no axis labels
+        left.setTextSize(15f);
+
+        chart.getAxisRight().setEnabled(false); // no right axis
+        chart.setDescription(null);
+        //left.addLimitLine(ll);
+        set.setColors(colors);
+        Legend legend = chart.getLegend();
+        //legend.setTextSize(20f);
+        chart.getLegend().setEnabled(false);
+        chart.setDrawBorders(true);
+        chart.invalidate(); // refresh
+
+    }
+
+    public void onClickBack(View view) {
+        Intent transitionToMainActivity= new Intent(this,MainActivity.class);
+        startActivity(transitionToMainActivity);
+    }
+
+
+    public void onClickLineChart(View view) {
+
+        Intent transitionToLineChart=new Intent(this,LineChartCalories.class);
+        startActivity(transitionToLineChart);
+
+
+    }
+}
